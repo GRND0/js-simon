e4e6f1c59d6540409bc1f3136247fa37fce9dd95
GRND0 <zefiro.turchetta@gmail.com>
2022-04-27 16:51:44 +0200

    array numeri utente

--- script.js
+++ script.js
@@ -1,17 +1,52 @@
 
-const grandezzaArray = 5 ;
-const arrayRandom = [] ;
-for (i=0; i < grandezzaArray; i++) {
-    arrayRandom.push(numeriRandom(0, 100))
+const grandezzaArray = 5;
+const numeriCasuali = [];
+for (i = 0; i < grandezzaArray; i++) {
+    numeriCasuali.push(numeriRandom(0, 100))
 }
-console.log(arrayRandom);
-document.getElementById("numeri").innerHTML = arrayRandom ;
+
+document.getElementById("numeri").innerHTML = numeriCasuali;
+
+
+setTimeout(function() {
+    nascondiNumeri();
+}, 30000);
+
+
+setTimeout(function() {
+    let numeriUtente = [] ;
+    let varProvvisoria = inserisciNumeri() ;
+    numeriUtente.push(varProvvisoria) ;
+    console.log(numeriUtente);
+    
 
 
 
+    
+
+}, 30010);
+
+
+
+
+//FUNZIONI
 function numeriRandom(minimo, massimo) {
     min = Math.ceil(minimo);
     max = Math.floor(massimo);
-    return Math.floor(Math.random() * (massimo - minimo + 1) + minimo); 
-    ;
+    return Math.floor(Math.random() * (massimo - minimo + 1) + minimo);
 }
+
+function nascondiNumeri() {
+    document.getElementById("numeri").innerHTML = "";
+}
+
+function inserisciNumeri() {
+    let numeroUtente;
+    const arrayUtente = [];
+    do {
+        numeroUtente = parseInt(prompt("Inserisci un numero"));
+        arrayUtente.push(numeroUtente);
+    } while (arrayUtente.length < 5);
+    
+    return arrayUtente ;
+}  
